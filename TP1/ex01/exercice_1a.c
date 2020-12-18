60098ede2ad4185b3c76cd6ae6c366b1c0c35c3e
SheimaeBnani <Chaimae.BNANI@um6p.ma>
2020-12-19 00:39:46 +0100

    Exercice_1a.c

--- TP1/ex01/exercice_1a.c
+++ TP1/ex01/exercice_1a.c
@@ -3,57 +3,60 @@
 #define N 10
 
 
-float *descente(float A[N][N], float B[N], int n)
+double *Descente(double A[N][N], double B[N], int n)
 {
-  float   *x;
+ 	  double *X;
+	  X = malloc(sizeof(double) * n);
+	  int i,j,k;
+          double s;
+	  X[0]=B[0]/A[0][0];
+          for(i = 1; i < n; i++)
+          {
+                s=0;
+                for(j = 0; j < i; j++)
+                        s += A[i][j]* X[j];
+                X[i] = (B[i]-s) / A[i][i];
 
-  /* Here we allocate the result verctor \
-  (you're gonna do it yourself in the next exercises)*/
-  x = malloc(sizeof(float) * n);
+          }
 
-
-  /******Implement the solution here******/
-  /***************************************/
-  
-  /****************************************/
-
-  return (x);
+           return (X);
 }
 
 
 int main()
 {
-  float   A[N][N], B[N];
-  float   *x;
-  int     n;
+  	double  A[N][N], B[N];
+  	double  *X;
+  	int   i,j,n;
 
-  printf("Enter the size of the matrix: ");
-  scanf("%d", &n);
+  	printf("Enter the size of the matrix: ");
+  	scanf("%d", &n);
 
   /* Filling the matrix A */
-  printf("Filling the matrix A\n");
-  for(int i = 0; i < n; i++)
-  {
-    for(int j = 0; j < n; j++)
-    {
-      printf("A[%d][%d] = ", i, j);
-      scanf("%f", &A[i][j]);
-    }
-  }
+  	printf("Filling the matrix A\n");
+  	for( i = 0; i < n; i++)
+  	{
+    		for(j = 0; j < n; j++)
+    		{
+      		printf("A[%d][%d] = ", i, j);
+      		scanf("%lf", &A[i][j]);
+    		}
+  	}
 
   /* Filling the vector B*/
-  printf("Filling the matrix B\n");
-  for (int i = 0; i < n; i++)
-  {
-    printf("B[%d] = ", i);
-    scanf("%f", &B[i]);
-  }
+  	printf("Filling the matrix B\n");
+  	for (i = 0; i < n; i++)
+  	{
+    		printf("B[%d] = ", i);
+    		scanf("%lf", &B[i]);
+  	}
 
   /* The calculation of the result */
-  x = descente(A, B, n);
+  	X = Descente(A, B, n);
   
   /* Printing the results */
-  printf("\nThe resulting vector: [");
-  for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+  	printf("\nThe solution is : ");
+        for (i = 0; i < n; i++)
+                 printf("%lf\n", X[i]);
+
 }
