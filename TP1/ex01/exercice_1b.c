5d8eba0118c89b1ec3061f8bea1bc18fe664970e
SheimaeBnani <Chaimae.BNANI@um6p.ma>
2020-12-19 00:56:13 +0100

    Exercice_1b.c

--- TP1/ex01/exercice_1b.c
+++ TP1/ex01/exercice_1b.c
@@ -2,55 +2,61 @@
 #include <stdlib.h>
 #define N 10
 
-float *remontee(float A[N][N], float B[N], int n)
+double *Remonte(double A[N][N], double B[N], int n)
 {
 
-  /* A hint: you cant return an array which is allocated in the stack memory. \
-  ** you may consider to create a dynamically allocated array and return a pointer to it. \
-  ** https://gribblelab.org/CBootCamp/7_Memory_Stack_vs_Heap.html
-  */
+        double *X;
+        X = malloc(sizeof(double) * n);
+        int i,j;
+        double s;
+        X[n-1] = B[n-1] / A[n-1][n-1];
+        for(i = n-2; i >= 0; i--)
+        {
+                s = 0;
+                for(j = i+1; j < n; j++)
+                        s = s + A[i][j] * X[j];
+                X[i] = (B[i] - s) / A[i][i];
+        }
 
-  /******Implement the solution here******/
-  /***************************************/
-  
-  /***************************************/
-  return x;
+  return (X);
 }
 
 int main()
 {
-  float   A[N][N], B[N];
-  float   *x;
-  int     n;
+  double   A[N][N], B[N];
+  double  *X;
+  int   i,j,n;
 
   printf("Enter the size of the matrix: ");
   scanf("%d", &n);
 
   /* Filling the matrix A */
   printf("Filling the matrix A\n");
-  for(int i = 0; i < n; i++)
+  for(i = 0; i < n; i++)
   {
-    for(int j = 0; j < n; j++)
+    for(j = 0; j < n; j++)
     {
       printf("A[%d][%d] = ", i, j);
-      scanf("%f", &A[i][j]);
+      scanf("%lf", &A[i][j]);
     }
   }
 
   /* Filling the vector B*/
   printf("Filling the vector B\n");
-  for (int i = 0; i < n; i++)
+  for (i = 0; i < n; i++)
   {
     printf("B[%d] = ", i);
-    scanf("%f", &B[i]);
+    scanf("%lf", &B[i]);
   }
 
   /* The calculation of the result */
-  x = remontee(A, B, n);
+  X = Remonte(A, B, n);
 
   /* Printing the results */
-  printf("\nThe resulting vector: [");
-  for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+  printf("\nThe solution is : ");
+        for (int i = 0; i < n; i++)
+                 printf("%lf\n", X[i]);
+
+
 }
 
