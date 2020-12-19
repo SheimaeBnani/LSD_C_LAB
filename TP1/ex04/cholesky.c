715c5fe0d28f8c631e673fff86419f096105d109
SheimaeBnani <Chaimae.BNANI@um6p.ma>
2020-12-19 23:10:32 +0100

    Cholesky.c

--- TP1/ex04/cholesky.c
+++ TP1/ex04/cholesky.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<math.h>
 #define N 10
 
 int main()
@@ -7,7 +8,8 @@ int main()
 	int i, j, k, n;
         printf("Enter the size of the matrix: ");
         scanf("%d", &n);
-        double C[N][N], A[N][N], B[N], S1, S2;
+        double C[N][N], A[N][N], B[N], X[N], S1, S2;
+        double T[N][N], IN[N][N], IT[N][N], Y[N], ratio, s;
         for(i=0;i<n;i++)
         {
                 for(j=0;j<n;j++)
@@ -41,54 +43,154 @@ int main()
                 }
         }
         printf("\n");
+	printf("C= ");
         for(i=0;i<n;i++)
         {
                 for(j=0;j<n;j++)
                         printf("%lf\t", C[i][j]);
                  printf("\n");
         }
-
-  	return (0);
-}
-/*double Determinant(double C[N][N], int n);
-{
-                double s = 1, det = 0, D[N][N];
-                int i,j,m,k,c;
-                if(n == 1)
-                {
-                        return (C[0][0]);
-                }
-                else
-                {
-               		 det = 0;
-               		 for(c=0;c<n;c++)
-                	{
-                       		 m = 0;
-                       		 k = 0;
-                       		 for(i=0;i<n;i++)
-                        	{
-                                	for(j=0;j<n;j++)
-                                	{	
-                                       		 D[i][j] = 0;
-                                        	if(i!=0 && j!=c)
-                                        	{
-                                               		 D[m][k] = C[i][j];
-                                               		 if(k<n-2)
-                                                       		 k++;
-                                               		 else
-                                               		 {
-                                                       		 k = 0;
-                                                       		 m++;
-                                                	}
-                                        	}
-                               		 }
-                        	}
-			det = det + s * (C[0][c] * determinant(D, n-1);
-			s = -1 * s;
-			}
+	printf("T= \n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			T[i][j] = C[j][i];
+			printf("%lf\t", T[i][j]);
 		}
-		return (det);
- }
+		printf("\n");
+	}
+	for(i=1;i<=n;i++)
+	{
+		 for(j=1;j<=n;j++)
+	         {
+			 if(i==j)
+		          {
+				   C[i][j+n] = 1;
+			  }
+			 else
+			 {
+				   C[i][j+n] = 0;
+			 }
+		 }
+	 }
+	for(i=1;i<=n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			 if(i!=j)
+			 {
+				 ratio = C[j][i]/C[i][i];
+				 for(k=1;k<=2*n;k++)
+				 {
+					  C[j][k] = C[j][k] - ratio*C[i][k];
+				  }
+			 }
+		 }
+	 }
+	for(i=1;i<=n;i++)
+	 {
+		 for(j=n+1;j<=2*n;j++)
+		 {
+			   	IN[i][j] = C[i][j]/C[i][i];
+		 }
+	 }
+	 /* The inverse of C */
+	 printf("\nThe inverse of C is:\n");
+	 for(i=1;i<=n;i++)
+	 {
+		  for(j=n+1;j<=2*n;j++)
+		  {
+			  printf("%lf\t",C[i][j]);
+		  }
+		  printf("\n");
+	 }
+	/*The inverse of T */
+         for(i=0;i<n;i++)
+         {
+                  for(j=0;j<n;j++)
+                  {
+                           if(i==j)
+                           {
+                                T[i][j+n] = 1;
+                           }
+                           else
+                           {
+                                T[i][j+n] = 0;
+                           }
+                  }
+         }
+         for(i=0;i<n;i++)
+        {
+                  for(j=0;j<n;j++)
+                  {
+                           if(i!=j)
+                           {
+                                    ratio= T[j][i]/T[i][i];
+                                    for(k=0;k<2*n;k++)
+                                    {
+                                        T[j][k] = T[j][k] - ratio*T[i][k];
+                                    }
+                           }
+                  }
+         }
+         for(i=0;i<n;i++)
+        {
+                  for(j=n;j<2*n;j++)
+                  {
+                        IT[i][j] = T[i][j]/T[i][i];
+                  }
+         }
+         printf("\nThe inverse T is:\n");
+         for(i=0;i<n;i++)
+         {
+                  for(j=n;j<2*n;j++)
+                  {
+                        printf("%lf\t",T[i][j]);
+                  }
+                  printf("\n");
+         }
+         /*Resulotion of the system*/
+         printf("Y=I*B:\n");
+         Y[0]=B[0]*IN[0][0];
+ 
+         for(i=1;i<n;i++)
+         {
+              s=0;
+              for(j=0;j<i;j++)
+                         s+=C[i][j]*Y[j];
+              Y[i]=(B[i]-s)*IN[i][i];
+         }
+         for(i=0;i<n;i++)
+                  printf("%lf\t", Y[i]);
+         printf("\n");
+         Y[0]=B[0]/C[0][0];
+         for(i=1;i<n;i++)
+         {
+                s=0;
+                for(j=0;j<i;j++)
+                {
+                      s+=C[i][j]*Y[j];
+                }
+                Y[i]=(B[i]-s)/C[i][i];
+         }
+         for(i=0;i<n;i++)
+         {
+               printf("Y[%d]=%lf\t", i, Y[i]);
+         }
+         X[n-1]=Y[n-1]/T[n-1][n-1];
+	for(int i=n-2;i>=0;i--)
+	{
+		s=0;
+		for(j=i;j<n;j++)
+			s=s+T[i][j]*X[j];
+		X[i]=(Y[i]-s)/T[i][i];
+	}
+         for(i=0;i<n;i++)
+         {
+               printf("X[%d]=%lf\t", i, X[i]);
+         }
 
+		 return(0);
+}
 
-*/
